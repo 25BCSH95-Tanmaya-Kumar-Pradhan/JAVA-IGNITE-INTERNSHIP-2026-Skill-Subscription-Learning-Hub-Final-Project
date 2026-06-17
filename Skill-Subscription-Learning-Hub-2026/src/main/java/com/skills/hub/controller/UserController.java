f55d0bd654fb6ada13629b914fc59c4149828cc3
25BCSH95-Tanmaya-Kumar-Pradhan <ultraaeonus@gmail.com>
2026-06-17 15:51:31 +0530

    Update UserController.java

--- Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/controller/UserController.java
+++ Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/controller/UserController.java
@@ -5,13 +5,6 @@ import com.skills.hub.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-/*
-=========================================================
-WHAT IS THIS FILE?
-Handles user actions like register and login
-=========================================================
-*/
-
 @Controller
 public class UserController {
 
@@ -24,50 +17,47 @@ public class UserController {
     @GetMapping("/register")
     public String showRegisterPage() {
 
-        // =========================
-        // TASK
-        // =========================
-        // STEP 1: Return register page
-
-        return null; // TODO: "register"
+        //Return register page
+        return "register";
     }
 
     @PostMapping("/register")
     public String registerUser(@ModelAttribute User user) {
 
-        // =========================
-        //TASK
-        // =========================
-        // STEP 1: call service.registerUser(user)
-        // STEP 2: if success → redirect to login
-        // STEP 3: else → stay on register page
+        
+        User savedUser = userService.registerUser(user);
 
-        return null;
+        
+        if (savedUser != null) {
+            return "redirect:/login";
+        }
+
+        return "register";
     }
 
     @GetMapping("/login")
     public String showLoginPage() {
 
-        // STEP 1: return login page
-
-        return null; // TODO: "login"
+        
+        return "login";
     }
 
     @PostMapping("/login")
     public String login(@RequestParam String email,
-                         @RequestParam String password) {
+                        @RequestParam String password) {
 
-        // =========================
-        // PSEUDO CODE
-        // =========================
-        // STEP 1: call userService.login(email, password)
-        // STEP 2: if user != null → redirect /packs
-        // STEP 3: else → return login page again
+        
+        User user = userService.login(email, password);
 
-        return null;
+        
+        if (user != null) {
+            return "redirect:/packs";
+        }
+
+        return "login";
     }
 
-	public UserService getUserService() {
-		return userService;
-	}
+    public UserService getUserService() {
+        return userService;
+    }
 }
